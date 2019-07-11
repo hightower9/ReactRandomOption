48c35587dd08c29526c1ea1b0c578b645150a786
alister <alister.messi@gmail.com>
2019-07-11 15:08:36 +0100

    change

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry:'./src/app.js',
     output:{
-        path: path.join(__dirname, './build'),
+        path: path.resolve(__dirname, './build'),
         filename:'bundle.js'
     },
     module:{
